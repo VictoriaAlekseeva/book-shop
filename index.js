4198b3f58c0e49045f7c3d8e576c8cd62d261b94
VictoriaAlekseeva <vicalekseeva88@gmail.com>
2022-11-25 16:16:58 +0100

    feat: add popup appearance with show more button

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const showMoreButton = document.querySelectorAll('.book__show-more');
+
+
 
 async function getBooksInfo() {
   const res = await fetch('./books.json');
   const data = await res.json();
-
-  console.log(data[1].author)
 
   for (let i = 0; i < data.length; i++) {
 
@@ -21,12 +20,9 @@ async function getBooksInfo() {
     const bookPrice = new createElem ('p', 'book__price', `$ ${data[i].price}`);
     const buttonAdd = new createElem ('button', 'button button__add-to-bag', 'Add to bag')
 
-    bookCard.append(bookCardImg, bookTitle, bookAuthor, bookShowMore, bookPrice, buttonAdd)
-
+    bookCard.append(bookCardImg, bookTitle, bookAuthor, bookShowMore, bookPrice, buttonAdd);
     }
 
-    // quote.textContent = data[quoteNumber].text;
-    // author.textContent = data[quoteNumber].author;
 };
 
 getBooksInfo();
@@ -68,6 +64,60 @@ welcome.prepend(welcomeText);
 
 const booksWrapper = new createBlock ('div', 'books__wrapper');
 welcome.after(booksWrapper);
+
+const popupWrapper = new createBlock ('div', 'popup__wrapper');
+booksWrapper.after(popupWrapper);
+
+const popup = new createBlock ('div', 'popup');
+popupWrapper.prepend(popup);
+
+const popupClose = new createElem('span', 'popup__close', 'x');
+popup.prepend(popupClose);
+
+const popupText = new createElem('p', 'popup__text', '');
+popupClose.after(popupText);
+
+
+
+booksWrapper.onclick = function(event) {
+  let target = event.target;
+
+  if (target.className != 'book__show-more') return;
+
+  let el = target.closest('.book__card').querySelector('.book__title').textContent;
+
+        event.preventDefault();
+        popup.classList.add('active');
+        popupWrapper.classList.add('active');
+        popupText.textContent = getBookDescription(el);
+        console.log(popupText.textContent);
+        return popupText.textContent;
+}
+
+async function getBookDescription(el) {
+  const res = await fetch('./books.json');
+  const data = await res.json();
+
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].title == el) {
+      console.log('data title', data[i].title, 'data description', data[i].description)
+      popupText.textContent = data[i].description;
+    }
+  }
+
+  return popupText.textContent;
+}
+
+popupClose.addEventListener('click', ()=> { // закрыть окно по клику на крестик
+  popup.classList.remove('active');
+  popupWrapper.classList.remove('active');
+});
+
+popupWrapper.addEventListener('click', (event) => { // закрыть окно по клику на фон
+  if (event.target.closest('.popup')) return;
+    popup.classList.remove('active');
+    popupWrapper.classList.remove('active');
+})
 
 
 
